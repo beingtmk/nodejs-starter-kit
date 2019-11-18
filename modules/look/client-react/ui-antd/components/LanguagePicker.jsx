775311d045d35e5e8259eeee7068006db621c2f7
Lokesh Rana <lokeshrana9999@gmail.com>
2019-11-19 01:44:36 +0530

    navbar refined

--- modules/look/client-react/ui-antd/components/LanguagePicker.jsx
+++ modules/look/client-react/ui-antd/components/LanguagePicker.jsx
@@ -1,30 +1,33 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import { Menu, Dropdown, Icon } from 'antd';
+import React from "react";
+import PropTypes from "prop-types";
+import { Menu, Dropdown, Icon } from "antd";
+
+const { SubMenu } = Menu;
 
 export default class LanguagePicker extends React.Component {
-  getMenuItems = () => (
-    <Menu onClick={({ key }) => this.props.i18n.changeLanguage(key)}>
-      {Object.keys(this.props.i18n.store.data).map(lang => (
-        <Menu.Item key={lang}>{lang.slice(0, 2).toUpperCase()}</Menu.Item>
-      ))}
-    </Menu>
-  );
-
   render() {
     const { i18n } = this.props;
     const languages = Object.keys(i18n.store.data);
 
     return (
-      <React.Fragment>
+      <Menu theme="dark" style={{ height: "32px" }} className="nav-translator">
         {languages.length > 1 && (
-          <Dropdown overlay={this.getMenuItems()}>
-            <a className="ant-dropdown-link" href="#">
-              {this.props.i18n.language.slice(0, 2).toUpperCase()} <Icon type="down" />
-            </a>
-          </Dropdown>
+          <SubMenu
+            key="sub1"
+            onClick={({ key }) => this.props.i18n.changeLanguage(key)}
+            title={
+              <div className="lang-picker-submenu">
+                <Icon type="read" />{" "}
+                {this.props.i18n.language.slice(0, 2).toUpperCase()}{" "}
+              </div>
+            }
+          >
+            {Object.keys(this.props.i18n.store.data).map(lang => (
+              <Menu.Item key={lang}>{lang.slice(0, 2).toUpperCase()}</Menu.Item>
+            ))}
+          </SubMenu>
         )}
-      </React.Fragment>
+      </Menu>
     );
   }
 }
