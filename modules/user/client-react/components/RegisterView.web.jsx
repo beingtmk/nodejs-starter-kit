3fa0f77beb103418efdb74e7880a6ead062e34dd
r-kohale9 <r.kohale9@gmail.com>
2019-12-15 18:03:39 +0530

    singup and profile page overflow dealt

--- modules/user/client-react/components/RegisterView.web.jsx
+++ modules/user/client-react/components/RegisterView.web.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Helmet from 'react-helmet';
+import Grid from 'hedron';
 
 import { translate } from '@gqlapp/i18n-client-react';
 import { LayoutCenter, PageLayout, Card, CardGroup, CardTitle, CardText } from '@gqlapp/look-client-react';
@@ -32,15 +33,29 @@ const RegisterView = ({ t, onSubmit, isRegistered }) => {
 
   return (
     <PageLayout>
-      {renderMetaData(t)}
-      <LayoutCenter>
-        <h1 className="text-center">{t('reg.form.title')}</h1>
-        {isRegistered && settings.auth.password.requireEmailConfirmation ? (
-          renderConfirmationModal()
-        ) : (
-          <RegisterForm onSubmit={onSubmit} />
-        )}
-      </LayoutCenter>
+      <Grid.Provider breakpoints={{ sm: '-500', md: '501-768', lg: '+769' }}>
+        <Grid.Bounds direction="vertical">
+          {renderMetaData(t)}
+          <Grid.Box sm={{ hidden: true }}>
+            <LayoutCenter>
+              <h1 className="text-center">{t('reg.form.title')}</h1>
+              {isRegistered && settings.auth.password.requireEmailConfirmation ? (
+                renderConfirmationModal()
+              ) : (
+                <RegisterForm onSubmit={onSubmit} />
+              )}
+            </LayoutCenter>
+          </Grid.Box>
+          <Grid.Box md={{ hidden: true }} lg={{ hidden: true }}>
+            <h1 className="text-center">{t('reg.form.title')}</h1>
+            {isRegistered && settings.auth.password.requireEmailConfirmation ? (
+              renderConfirmationModal()
+            ) : (
+              <RegisterForm onSubmit={onSubmit} />
+            )}
+          </Grid.Box>
+        </Grid.Bounds>
+      </Grid.Provider>
     </PageLayout>
   );
 };
