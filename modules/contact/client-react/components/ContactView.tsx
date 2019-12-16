716a71fa71d5685f80c33a5018ce7cc37e2f7beb
r-kohale9 <r.kohale9@gmail.com>
2019-12-16 20:33:25 +0530

    contact, pagination responsive layout

--- modules/contact/client-react/components/ContactView.tsx
+++ modules/contact/client-react/components/ContactView.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Grid from 'hedron';
 import Helmet from 'react-helmet';
 
 import { TranslateFunction } from '@gqlapp/i18n-client-react';
@@ -15,16 +16,33 @@ interface ContactViewProps {
 
 const ContactView = (props: ContactViewProps) => {
   const { t } = props;
+  const renderContent = () => (
+    <>
+      <h1 className="text-center">{t('form.title')}</h1>
+      <ContactForm {...props} />
+    </>
+  );
   return (
     <PageLayout>
-      <Helmet
-        title={`${settings.app.name} - ${t('title')}`}
-        meta={[{ name: 'description', content: `${settings.app.name} - ${t('meta')}` }]}
-      />
-      <LayoutCenter>
-        <h1 className="text-center">{t('form.title')}</h1>
-        <ContactForm {...props} />
-      </LayoutCenter>
+      <Grid.Provider breakpoints={{ sm: '-500', md: '501-768', lg: '+769' }}>
+        <Grid.Bounds direction="vertical">
+          <Helmet
+            title={`${settings.app.name} - ${t('title')}`}
+            meta={[
+              {
+                name: 'description',
+                content: `${settings.app.name} - ${t('meta')}`
+              }
+            ]}
+          />
+          <Grid.Box sm={{ hidden: 'true' }}>
+            <LayoutCenter>{renderContent()}</LayoutCenter>
+          </Grid.Box>
+          <Grid.Box md={{ hidden: 'true' }} lg={{ hidden: 'true' }}>
+            {renderContent()}
+          </Grid.Box>
+        </Grid.Bounds>
+      </Grid.Provider>
     </PageLayout>
   );
 };
