22dcd29699cdb3a445f585286d76eddc02156a08
r-kohale9 <r.kohale9@gmail.com>
2019-12-23 00:18:25 +0530

    renderAddress done with styled component with backgroundColor and borderColor as default props

--- modules/look/client-react/ui-antd/components/RenderAddress.jsx
+++ modules/look/client-react/ui-antd/components/RenderAddress.jsx
@@ -1,60 +1,106 @@
 import React from 'react';
 import Grid from 'hedron';
 import PropTypes from 'prop-types';
-// import styled, { css } from 'styled-components';
+import styled, { css } from 'styled-components';
 import { RenderField } from '@gqlapp/look-client-react';
 import { FieldAdapter as Field } from '@gqlapp/forms-client-react';
 import { Icon, Button, Form, Modal, Popconfirm, message } from 'antd';
 
 const FormItem = Form.Item;
 
-// const AddressFormItem = styled.div`
-//   display: inline-block;
-//   margin: 0px 5px;
-//   width: 75%;
-// `;
+const AddressFormItem = styled.div`
+  display: inline-block;
+  margin: 0px 5px;
+  width: 75%;
+`;
 
-// const HomeAddressBlock = styled.div`
-//   font-size: 14px;
-//   text-shadow: 0.7px 0 0;
-//   letter-spacing: 0.3px;
-//   position: relative;
-//   bottom: 8px;
-// `;
+const HomeAddressBlock = styled.div`
+  font-size: 14px;
+  text-shadow: 0.7px 0 0;
+  letter-spacing: 0.3px;
+  position: relative;
+  bottom: 8px;
+`;
 
-// const AddressLines = styled.div`
-//   line-height: 12px;
-//   color: #5d5f5f !important;
-//   position: relative;
-//   bottom: 15px;
-// `;
+const AddressLines = styled.h4`
+  & > h4 {
+    line-height: 12px;
+    color: #5d5f5f !important;
+    position: relative;
+    bottom: 15px;
+  }
+`;
 
-// const HomeAddress = styled.div`
-//   font-size: 12px;
-//   padding: 20px;
-//   padding-bottom: 1px;
-//   border: 3px solid rgba(175, 226, 217, 0.81);
-//   background-color: rgba(35, 177, 149, 0.2);
-//   width: 100%;
-//   border-radius: 10px;
-// `;
+const AddEditbtn = styled.div`
+  position: relative;
+  bottom: 10px;
+  right: 6px;
+`;
 
-// const AddNewAddressBlock = styled.div`
-//   width: 100%;
-//   text-align: center;
-//   border: 1px solid #ddd;
-//   padding: 35px 20px;
-//   cursor: pointer;
-//   border-radius: 10px;
-//   height: 215px;
-//   background-color: white;
-//   width: 300px !important;
+const AddDelbtn = styled.div`
+  position: relative;
+  bottom: 10px;
+  left: 6px;
+`;
 
-//   &:hover && {
-//     border: 3px solid rgba(175, 226, 217, 0.81);
-//     width: 300px !important;
-//   }
-// `;
+const HomeAddress = styled.div`
+  font-size: 12px;
+  padding: 20px;
+  padding-bottom: 1px;
+  border: 3px solid ${props => props.borderColor || 'rgba(175, 226, 217, 0.81)'};
+  background-color: ${props => props.backgroundColor || 'rgba(35, 177, 149, 0.2)'};
+  width: 100%;
+  border-radius: 10px;
+
+  ${props =>
+    props.selectable
+      ? css`
+          margin-bottom: 15px;
+          background-color: #fff;
+          width: 275px !important;
+        `
+      : css`
+          margin-top: 15px;
+          width: 275px !important;
+        `}
+  ${props =>
+    props.active &&
+    css`
+      cursor: pointer;
+      background-color: ${props => props.backgroundColor || 'rgba(35, 177, 149, 0.2)'};
+    `}
+  &:hover {
+    background-color: ${props => props.selectable && (props.backgroundColor || 'rgba(35, 177, 149, 0.2)')};
+  }
+`;
+
+const AddNewAddressBlock = styled.div`
+  width: 100%;
+  text-align: center;
+  border: 1px solid #ddd;
+  padding: 35px 20px;
+  cursor: pointer;
+  border-radius: 10px;
+  height: 215px;
+  background-color: white;
+  width: 275px !important;
+
+  &:hover {
+    border: ${props => '3px solid' + props.borderColor || '3px solid rgba(175, 226, 217, 0.81)'};
+    width: 275px !important;
+  }
+`;
+
+const AddNewAddress = styled.div`
+  font-size: 70px;
+  line-height: 75px;
+  text-shadow: 1px 0 0;
+  color: #ddd;
+`;
+
+const PadB30 = styled.div`
+  padding-bottom: 30px;
+`;
 
 class RenderAddress extends React.Component {
   constructor(props) {
@@ -78,9 +124,9 @@ class RenderAddress extends React.Component {
 
   classNamesgroup(e) {
     if (this.state.address === e) {
-      return 'Active-Cond HomeAddress';
+      return 'Active-Cond';
     } else {
-      return 'Not-Active HomeAddress';
+      return 'Not-Active';
     }
   }
 
@@ -122,7 +168,7 @@ class RenderAddress extends React.Component {
   };
 
   render() {
-    const { arrayHelpers, name, addresses, t, label, onSubmit } = this.props;
+    const { arrayHelpers, name, addresses, t, label, onSubmit, backgroundColor, borderColor } = this.props;
     function cancel(e) {
       console.log(e);
       message.error('Click on No');
@@ -146,40 +192,39 @@ class RenderAddress extends React.Component {
       addresses.map(
         (address, indexa) => (
           (formItems[indexa] = keys.map((k, indexk) => (
-            <FormItem className="addressFormItem" key={indexk}>
-              <Field
-                name={`${name}[${indexa}].${k.key}`}
-                component={RenderField}
-                placeholder={k.label}
-                type="text"
-                label={t(`${label}.${k.label}`)}
-                value={address[k.key]}
-              />
-            </FormItem>
+            <AddressFormItem>
+              <FormItem key={indexk}>
+                <Field
+                  name={`${name}[${indexa}].${k.key}`}
+                  component={RenderField}
+                  placeholder={k.label}
+                  type="text"
+                  label={t(`${label}.${k.label}`)}
+                  value={address[k.key]}
+                />
+              </FormItem>
+            </AddressFormItem>
           ))),
           //Geting all the addressCard.
           (addressCard[indexa] = (
             <>
-              <div className="HomeAddressBlock"> Home Address </div>
+              <HomeAddressBlock> Home Address </HomeAddressBlock>
               <br />
-              <div className="addressLines">
+              <AddressLines>
                 <h4>{address.streetAddress1 && address.streetAddress1 + ','}</h4>
                 <h4>{address.streetAddress2 && address.streetAddress2 + ','}</h4>
                 <h4>{address.city && address.city + ','}</h4>
                 <h4>{address.state && address.state + ','}</h4>
                 <h4>{address.pinCode && address.pinCode + ','}</h4>
-              </div>
+              </AddressLines>
 
               <Grid.Bounds direction="horizontal">
                 <Grid.Box>
-                  <Button
-                    className="addEditbtn"
-                    shape="circle"
-                    size="large"
-                    onClick={() => this.modalControl(indexa, true)}
-                  >
-                    <Icon type="edit" />
-                  </Button>
+                  <AddEditbtn>
+                    <Button shape="circle" size="large" onClick={() => this.modalControl(indexa, true)}>
+                      <Icon type="edit" />
+                    </Button>
+                  </AddEditbtn>
                   <Modal
                     visible={this.state.visible[indexa]}
                     title="Address"
@@ -202,9 +247,11 @@ class RenderAddress extends React.Component {
                     okText="Yes"
                     cancelText="No"
                   >
-                    <Button className="addDelbtn" type="danger" shape="circle" size="large">
-                      <Icon type="delete" />
-                    </Button>
+                    <AddDelbtn>
+                      <Button type="danger" shape="circle" size="large">
+                        <Icon type="delete" />
+                      </Button>
+                    </AddDelbtn>
                   </Popconfirm>
                 </Grid.Box>
               </Grid.Bounds>
@@ -228,38 +275,49 @@ class RenderAddress extends React.Component {
             </Grid.Box>
             <Grid.Box>
               <Grid.Bounds direction="vertical">
-                <Grid.Box className="PadB30">
-                  {isSelectable
-                    ? addresses.map((address, indexas) => (
-                        <Grid.Box>
-                          <div
-                            key={indexas}
-                            onClick={() => this.renderCondition(indexas)}
-                            className={this.classNamesgroup(indexas) + ' addis-Sel'}
-                          >
-                            {addressCard[indexas]}
-                          </div>
-                        </Grid.Box>
-                      ))
-                    : addresses.map((address, indexas) => (
-                        <Grid.Box>
-                          <div className="HomeAddress addnot-Sel" key={indexas}>
-                            {addressCard[indexas]}
-                          </div>
-                        </Grid.Box>
-                      ))}
+                <Grid.Box>
+                  <PadB30>
+                    {isSelectable
+                      ? addresses.map((address, indexas) => (
+                          <Grid.Box>
+                            <HomeAddress
+                              backgroundColor={backgroundColor}
+                              borderColor={borderColor}
+                              key={indexas}
+                              selectable={true}
+                              onClick={() => this.renderCondition(indexas)}
+                              active={this.state.address === indexas ? true : false}
+                            >
+                              {addressCard[indexas]}
+                            </HomeAddress>
+                          </Grid.Box>
+                        ))
+                      : addresses.map((address, indexas) => (
+                          <Grid.Box>
+                            <HomeAddress
+                              backgroundColor={backgroundColor}
+                              borderColor={borderColor}
+                              selectable={false}
+                              key={indexas}
+                            >
+                              {addressCard[indexas]}
+                            </HomeAddress>
+                          </Grid.Box>
+                        ))}
+                  </PadB30>
                 </Grid.Box>
-                <Grid.Box className="PadB30">
-                  <div
-                    className="AddNewAddressBlock"
-                    onClick={this.handleAddAddress}
-                    style={{ marginTop: !isSelectable ? '15px' : null }}
-                  >
-                    <div className="AddNewAddress">
-                      <Icon type="plus" />
-                    </div>
-                    <h4 style={{ position: 'relative', top: '30px' }}>Add a new address</h4>
-                  </div>
+                <Grid.Box>
+                  <PadB30>
+                    <AddNewAddressBlock
+                      onClick={this.handleAddAddress}
+                      style={{ marginTop: !isSelectable ? '15px' : null }}
+                    >
+                      <AddNewAddress>
+                        <Icon type="plus" />
+                      </AddNewAddress>
+                      <h4 style={{ position: 'relative', top: '30px' }}>Add a new address</h4>
+                    </AddNewAddressBlock>
+                  </PadB30>
                 </Grid.Box>
               </Grid.Bounds>
             </Grid.Box>
@@ -281,5 +339,7 @@ RenderAddress.propTypes = {
   isSelectable: PropTypes.bool,
   arrayHelpers: PropTypes.object,
   onSubmit: PropTypes.func,
-  onSelect: PropTypes.func
+  onSelect: PropTypes.func,
+  backgroundColor: PropTypes.string,
+  borderColor: PropTypes.string
 };
