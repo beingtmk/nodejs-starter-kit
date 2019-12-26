6104aef0e2470c462b9bd66ed7b60da895dda2a6
r-kohale9 <r.kohale9@gmail.com>
2019-12-26 21:46:07 +0530

    frontend: Fixed Header, Fixed Footer & scrollbar tweaks remaning

--- modules/notifications/client-react/components/Notifications.web.jsx
+++ modules/notifications/client-react/components/Notifications.web.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Grid from 'hedron';
 // import PropTypes from 'prop-types';
-import styled from 'styled-components'; // , { css }
+import styled, { css } from 'styled-components'; // , { css }
 import {
   Icon,
   List
@@ -13,32 +13,83 @@ import UserAvatar from '@gqlapp/user-client-react/containers/UserAvatar';
 
 // import Notification from './Notification';
 
-const Notif = styled.div`
+const NotifAttr = css`
   width: 430px;
-  height: auto;
-  /* height: 430px; */
-  display: ${props => props.visible};
-
   background: #fff;
   border: 1px solid rgba(100, 100, 100, 0.4);
   border-radius: 0 0 2px 2px;
-  box-shadow: 0 3px 8px rgba(0, 0, 0, 0.25);
   color: #1d2129;
+`;
+
+const ScrollBar = css`
+  ::-webkit-scrollbar {
+    width: 5px;
+    display: none;
+
+    &-track {
+      background: #f1f1f1;
+      border-radius: 5px;
+
+      // &-piece {
+      //   background: ;
+      // }
+    }
+
+    &-thumb {
+      background: #888;
+      border-radius: 5px;
+
+      &:hover {
+        background: #00152a;
+        // width: 7px;
+        // display: initial;
+      }
+    }
+  }
+`;
+
+const Notif = styled.div`
+  height: 430px;
+  display: ${props => props.visible};
+
+  overflow-x: auto;
+  ${ScrollBar}
+
+  ${NotifAttr}
+  box-shadow: 0 3px 8px rgba(0, 0, 0, 0.25);
+
   position: absolute;
   right: 0;
-  z-index: 999;
+  z-index: 1;
 `;
 
 const NotifHeader = styled(Grid.Bounds)`
+  width: 428px;
+
+  ${NotifAttr}
+
   padding: 0px 0px 0px 5px;
   border-bottom: 1px solid #aaa;
   height: 40px;
+  position: fixed;
+  z-index: 2;
+`;
+
+const NotifHeaders = styled(Grid.Box)`
   position: relative;
-  top: -10px;
+  top: -6px;
 `;
 
 const NotifFooter = styled(Grid.Bounds)`
+  width: 428px;
+
+  ${NotifAttr}
+
   border-top: 1px solid #aaa;
+  z-index: 2;
+  position: fixed;
+  width: 430px;
+  top: 454px;
 `;
 
 const Uonhover = styled.a`
@@ -53,12 +104,13 @@ const ArrowUp = styled.div`
   width: 0;
   height: 0;
   display: ${props => props.visible};
-  border-left: 14px solid transparent;
-  border-right: 14px solid transparent;
-  border-bottom: 14px solid white;
+  border-left: 13px solid transparent;
+  border-right: 13px solid transparent;
+  border-bottom: 13px solid white;
   position: absolute;
-  top: 36px;
-  left: 14px;
+  top: 40px;
+  left: 16px;
+  z-index: 3;
 `;
 
 const AntListItemMeta = styled(List.Item.Meta).attrs({
@@ -71,9 +123,13 @@ const ContentDiv = styled.div`
   margin-right: 25px;
 `;
 
-// const NotifBody = styled(List)`
-//   /* overflow-x: auto; */
-// `;
+// .attrs({
+//   className: 'ant-list-items'
+// })
+const NotifBody = styled.div`
+  margin-top: 40px;
+  margin-bottom: 25px;
+`;
 
 const data = [
   {
@@ -143,17 +199,17 @@ class Notifications extends React.Component {
   notifHeader = () => {
     return (
       <NotifHeader direction="horizontal">
-        <Grid.Box>
+        <NotifHeaders>
           <h4>
             <b>Notifications</b>
           </h4>
-        </Grid.Box>
-        <Grid.Box shiftRight>
+        </NotifHeaders>
+        <NotifHeaders shiftRight>
           <Uonhover href="#">Mark all as read</Uonhover>
-        </Grid.Box>
-        <Grid.Box center>
+        </NotifHeaders>
+        <NotifHeaders center>
           <Uonhover href="#">Settings</Uonhover>
-        </Grid.Box>
+        </NotifHeaders>
       </NotifHeader>
     );
   };
@@ -161,25 +217,27 @@ class Notifications extends React.Component {
   notifBody = () => {
     // return <div>hello, world</div>;
     return (
-      <List
-        dataSource={this.state.data}
-        renderItem={item => (
-          <List.Item key={item.id}>
-            <AntListItemMeta
-              avatar={<UserAvatar src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" />}
-              title={<a href="#">{item.name.last}</a>}
-              description={item.email}
-            />
-            <ContentDiv>Content</ContentDiv>
-          </List.Item>
-        )}
-      >
-        {/* {this.state.loading && this.state.hasMore && (
+      <NotifBody>
+        <List
+          dataSource={this.state.data}
+          renderItem={item => (
+            <List.Item key={item.id}>
+              <AntListItemMeta
+                avatar={<UserAvatar src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" />}
+                title={<a href="#">{item.name.last}</a>}
+                description={item.email}
+              />
+              <ContentDiv>Content</ContentDiv>
+            </List.Item>
+          )}
+        >
+          {/* {this.state.loading && this.state.hasMore && (
           <div className="demo-loading-container">
             <Spin />
           </div>
         )} */}
-      </List>
+        </List>
+      </NotifBody>
     );
   };
 
