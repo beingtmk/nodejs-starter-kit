f331864fd093eaddeae408946f3d41893217a85a
Lokesh Rana <lokeshrana9999@gmail.com>
2019-12-29 15:29:35 +0530

    profile page implemented

--- modules/user/client-react/components/ProfileView.web.jsx
+++ modules/user/client-react/components/ProfileView.web.jsx
@@ -1,98 +1,197 @@
 import React from 'react';
-import Grid from 'hedron';
 import PropTypes from 'prop-types';
 import Helmet from 'react-helmet';
 import { Link } from 'react-router-dom';
-
 import { StripeSubscriptionProfile } from '@gqlapp/payments-client-react';
 import { translate } from '@gqlapp/i18n-client-react';
-import { LayoutCenter, Card, CardGroup, CardTitle, CardText, PageLayout } from '@gqlapp/look-client-react';
-import settings from '@gqlapp/config';
+import {
+  Card,
+  //  CardGroup,
+  CardText,
+  //  CardTitle,
+  PageLayout
+} from '@gqlapp/look-client-react';
+import { Row, Col, Divider, Icon, Button, Spin as Loader } from 'antd';
+// import UserVerificationsComponent from './verification/UserVerificationsComponent';
+import ProfileHeadComponent from './ProfileHeadComponent';
+// import UsersCardComponent from './UsersCardComponent';
+import settings from '../../../../settings';
+import AddressCardComponent from './AddressCardComponent';
+import userCardData from '../helpers/userCardData';
 
-const renderMetaData = t => {
-  return (
-    <Helmet
-      title={`${settings.app.name} - ${t('profile.title')}`}
-      meta={[
-        {
-          name: 'description',
-          content: `${settings.app.name} - ${t('profile.meta')}`
-        }
-      ]}
-    />
-  );
-};
+// To Do Abstract Out
 
-const ProfileView = ({ currentUserLoading, currentUser, t }) => {
-  const renderContent = () => (
-    <>
-      <h1 className="text-center">{t('profile.card.title')}</h1>
-      <Card>
-        <CardGroup>
-          <CardTitle>{t('profile.card.group.name')}:</CardTitle>
-          <CardText>{currentUser.username}</CardText>
-        </CardGroup>
-        <CardGroup>
-          <CardTitle>{t('profile.card.group.email')}:</CardTitle>
-          <CardText>{currentUser.email}</CardText>
-        </CardGroup>
-        <CardGroup>
-          <CardTitle>{t('profile.card.group.role')}:</CardTitle>
-          <CardText>{currentUser.role}</CardText>
-        </CardGroup>
-        {currentUser.profile && currentUser.profile.fullName && (
-          <CardGroup>
-            <CardTitle>{t('profile.card.group.full')}:</CardTitle>
-            <CardText>{currentUser.profile.fullName}</CardText>
-          </CardGroup>
-        )}
-        {/* Credit card info (Stripe subscription module)*/}
-        {settings.stripe.subscription.enabled &&
-          settings.stripe.subscription.publicKey &&
-          currentUser.role === 'user' && <StripeSubscriptionProfile />}
-      </Card>
-      <Link className="mt-2 btn user-link" to={`/users/${currentUser.id}`}>
-        {t('profile.editProfileText')}
-      </Link>
-    </>
-  );
-  if (currentUserLoading && !currentUser) {
+class ProfileView extends React.Component {
+  getUserId = () => {
+    let id = 0;
+    if (this.props.match) {
+      id = Number(this.props.match.params.id);
+    } else if (this.props.navigation) {
+      id = Number(this.props.navigation.state.params.id);
+    }
+    return id;
+  };
+
+  renderMetaData = t => {
     return (
-      <PageLayout>
-        {renderMetaData(t)}
-        <div className="text-center">{t('profile.loadMsg')}</div>
-      </PageLayout>
+      <Helmet
+        title={`${settings.app.name} - ${t('profile.title')}`}
+        meta={[
+          {
+            name: 'description',
+            content: `${settings.app.name} - ${t('profile.meta')}`
+          }
+        ]}
+      />
     );
-  } else if (currentUser) {
-    return (
-      <PageLayout>
-        <Grid.Provider breakpoints={{ sm: '-500', md: '501-768', lg: '+769' }}>
-          <Grid.Bounds direction="vertical">
-            {renderMetaData(t)}
-            <Grid.Box sm={{ hidden: true }}>
-              <LayoutCenter>{renderContent()}</LayoutCenter>
-            </Grid.Box>
-            <Grid.Box md={{ hidden: true }} lg={{ hidden: true }}>
-              {renderContent()}
-            </Grid.Box>
-          </Grid.Bounds>
-        </Grid.Provider>
-      </PageLayout>
-    );
-  } else {
-    return (
-      <PageLayout>
-        {renderMetaData(t)}
-        <h2>{t('profile.errorMsg')}</h2>
-      </PageLayout>
-    );
+  };
+
+  render() {
+    const { t } = this.props;
+    const { currentUser, currentUserLoading } = this.props;
+
+    if (currentUserLoading && !currentUser) {
+      return (
+        <PageLayout select="/profile">
+          <Loader text={t('profile.loadMsg')} />
+        </PageLayout>
+      );
+    } else if (currentUser) {
+      return (
+        <PageLayout select="/profile">
+          <h2 style={{ fontSize: '23px', fontWeight: 'bold', height: '61px', marginBottom: '0px' }}>
+            <Icon type="user" /> My Profile
+          </h2>
+          <Row gutter={5}>
+            <Col xs={{ span: 24 }} lg={{ span: 16 }}>
+              <Card>
+                <div align="right">
+                  <Link to={`/users/${currentUser.id}`}>
+                    <Button shape="circle" size="large">
+                      <Icon type="edit" />
+                    </Button>
+                  </Link>
+                </div>
+
+                <ProfileHeadComponent
+                  profile={currentUser.profile && currentUser.profile}
+                  description={userCardData(t, currentUser, this.getUserId()).profileHead}
+                />
+                <Divider />
+                <Row type="flex" justify="space-around" align="middle">
+                  <Col align="left" style={{ borderRight: '2px solid #23B195' }} span={12}>
+                    <div>
+                      <h2>
+                        <Icon type="user" /> {t('profile.card.group.name')}:
+                      </h2>
+                      <CardText>{currentUser.username}</CardText>
+                    </div>
+                    <div>
+                      <h2>
+                        <Icon type="solution" /> {t('profile.card.group.about')}:
+                      </h2>
+
+                      <CardText>
+                        {currentUser.profile && currentUser.profile.about ? currentUser.profile.about : 'Not Provided'}
+                      </CardText>
+                    </div>
+
+                    <div>
+                      <h2>
+                        <Icon type="team" /> {t('profile.card.group.role')}:
+                      </h2>
+
+                      <CardText>{currentUser.role ? currentUser.role : 'Not Provided'}</CardText>
+                    </div>
+
+                    {/* Portfolios */}
+                    <h2>
+                      <Icon type="paper-clip" /> {t('profile.card.group.portfolios.title')}
+                    </h2>
+                    {currentUser.portfolios && currentUser.portfolios.length !== 0
+                      ? currentUser.portfolios.map((portfolio, key) => (
+                          <div key={key}>
+                            <CardText>
+                              {portfolio.platform} : {portfolio.portfolioUrl}
+                            </CardText>
+                          </div>
+                        ))
+                      : 'Not Provided'}
+                  </Col>
+                  <Col align="right" span={12}>
+                    <div>
+                      <h2>
+                        <Icon type="mail" /> {t('profile.card.group.email')}:
+                      </h2>
+
+                      <CardText>{currentUser.email ? currentUser.email : 'Not Provided'}</CardText>
+                    </div>
+
+                    <div>
+                      <h2>
+                        <Icon type="shake" /> Mobile
+                      </h2>
+                      <CardText>
+                        {currentUser.profile && currentUser.profile.mobile
+                          ? currentUser.profile.mobile
+                          : 'Not Provided'}
+                      </CardText>
+                    </div>
+                  </Col>
+                </Row>
+                <Divider />
+                <h2>
+                  <Icon type="contacts" /> {t('profile.card.group.addresses.title')}
+                </h2>
+                <Row gutter={10}>
+                  {currentUser.addresses && currentUser.addresses.length !== 0
+                    ? currentUser.addresses.map((address, key) => (
+                        <Col xs={{ span: 24 }} md={{ span: 12 }} key={key}>
+                          <AddressCardComponent
+                            address={address}
+                            subTitle={t('profile.card.group.addresses.subTitle')}
+                            index={key}
+                          />
+                        </Col>
+                      ))
+                    : 'Not Provided'}
+                </Row>
+
+                {/* Credit card info (Stripe subscription module)*/}
+                {settings.stripe.subscription.enabled &&
+                  settings.stripe.subscription.publicKey &&
+                  currentUser.role === 'user' && <StripeSubscriptionProfile />}
+              </Card>
+            </Col>
+            {/* <Col xs={{ span: 24 }} lg={{ span: 8 }}>
+              <Row gutter={10} type="flex" justify="space-around" align="middle">
+                
+                <Col xs={{ span: 24 }} md={{ span: 8 }} lg={{ span: 24 }} style={{ height: '100%' }}>
+                  <UserVerificationsComponent
+                    data={currentUser.verification}
+                    verification={userCardData(t, currentUser, this.getUserId()).verification}
+                  />
+                </Col>
+              </Row>
+            </Col> */}
+          </Row>
+        </PageLayout>
+      );
+    } else {
+      return (
+        <PageLayout>
+          <h2>{t('profile.errorMsg')}</h2>
+        </PageLayout>
+      );
+    }
   }
-};
+}
 
 ProfileView.propTypes = {
   currentUserLoading: PropTypes.bool,
   currentUser: PropTypes.object,
-  t: PropTypes.func
+  t: PropTypes.func,
+  match: PropTypes.object,
+  navigation: PropTypes.object
 };
-
 export default translate('user')(ProfileView);
--- modules/user/client-react/containers/Profile.jsx
+++ modules/user/client-react/containers/Profile.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { graphql } from 'react-apollo';
+import PropTypes from 'prop-types';
 
 import { compose } from '@gqlapp/core-common';
 
@@ -7,7 +8,163 @@ import ProfileView from '../components/ProfileView';
 
 import CURRENT_USER_QUERY from '../graphql/CurrentUserQuery.graphql';
 
-const Profile = props => <ProfileView {...props} />;
+const Profile = props => {
+  console.log('profile', props);
+
+  const { currentUser } = props;
+  const { profile } = currentUser;
+  const profile_data = {
+    id: currentUser.id,
+    username: currentUser.role,
+    role: currentUser.role,
+    isActive: currentUser.isActive,
+    email: currentUser.id,
+    profile: {
+      firstName: profile.firstName,
+      lastName: profile.lastName,
+      isVerified: true,
+      isAvailable: true,
+      website: 'www.google.com',
+      about: 'web developer',
+      designation: 'dev',
+      avatar:
+        'https://images.unsplash.com/photo-1518806118471-f28b20a1d79d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=700&q=80',
+      rating: 5,
+      responseTime: 35,
+      acceptanceRate: 35,
+      mobile: 8888888888,
+      flag: 'good'
+    },
+
+    photoId: {
+      image:
+        'https://images.unsplash.com/photo-1518806118471-f28b20a1d79d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=700&q=80',
+      isVerified: true,
+      error: null
+    },
+    addresses: [
+      {
+        streetAddress1: 'aaaaaaaa',
+        streetAddress2: 'bbbbbbb',
+        city: 'ccccc',
+        state: 'dddddd',
+        pinCode: 'eeeeee'
+      },
+      {
+        streetAddress1: 'wwaaaaaa',
+        streetAddress2: 'wwwbbbbb',
+        city: 'wwccc',
+        state: 'wwdddd',
+        pinCode: 'wweeee'
+      }
+    ],
+
+    identification: {
+      type: 'government',
+      documentUrl:
+        'https://images.unsplash.com/photo-1518806118471-f28b20a1d79d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=700&q=80',
+      isVerified: true,
+      error: null
+    },
+    verification: {
+      isEmailVerified: true,
+      isMobileVerified: false,
+      isPhotoIdVerified: false,
+      isAddressVerified: false,
+      isIdVerified: true,
+      isReferred: false
+    },
+    endorsements: [
+      {
+        id: 5,
+        username: 'laluch',
+        profile: {
+          firstName: 'of',
+          lastName: 'rebellion',
+          avatar: null
+        }
+      }
+    ],
+    endorsed: [
+      {
+        id: 5,
+        username: 'laluch',
+        profile: {
+          firstName: 'of',
+          lastName: 'rebellion',
+          avatar: null
+        }
+      }
+    ],
+    followers: [
+      {
+        id: 5,
+        username: 'laluch',
+        profile: {
+          firstName: 'of',
+          lastName: 'rebellion',
+          avatar: null
+        }
+      }
+    ],
+    following: [
+      {
+        id: 5,
+        username: 'laluch',
+        profile: {
+          firstName: 'of',
+          lastName: 'rebellion',
+          avatar: null
+        }
+      }
+    ],
+
+    portfolios: [
+      {
+        platform: 'google',
+        portfolioUrl: 'www.google.com'
+      }
+    ],
+    // authCertificate {
+    //   serial
+    // }
+    // authFacebook {
+    //   fbId
+    //   displayName
+    // }
+    // authGoogle {
+    //   googleId
+    //   displayName
+    // }
+    // authGithub {
+    //   ghId
+    //   displayName
+    // }
+    // authLinkedin {
+    //   lnId
+    //   displayName
+    // }
+    createdAt: currentUser.createdAt,
+    updatedAt: currentUser.updatedAt
+  };
+
+  return <ProfileView {...props} currentUser={profile_data} />;
+};
+
+Profile.propTypes = {
+  currentUser: PropTypes.object
+  // shape({
+  //   id: PropTypes.number,
+  //   role: PropTypes.string,
+  //   isActive: PropTypes.bool,
+  //   createdAt: PropTypes.string,
+  //   updatedAt: PropTypes.string,
+  //   profile: PropTypes.shape({
+  //     firstName: PropTypes.string,
+  //     lastName: PropTypes.string
+  //   })
+  // })
+};
 
 export default compose(
   graphql(CURRENT_USER_QUERY, {
