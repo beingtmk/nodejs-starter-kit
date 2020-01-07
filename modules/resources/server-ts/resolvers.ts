7f3d262432fb95fc579f38937b1417e2d9748a6a
r-kohale9 <r.kohale9@gmail.com>
2020-01-07 18:05:08 +0530

    Backend: Resources all queries, mutation and subscription added

--- modules/resources/server-ts/resolvers.ts
+++ modules/resources/server-ts/resolvers.ts
@@ -1,39 +1,143 @@
 import { PubSub, withFilter } from 'graphql-subscriptions';
 import withAuth from 'graphql-auth';
 
-import { Resource } from './sql';
+import { Resource, Identifier } from './sql';
 
+interface Edges {
+  cursor: number;
+  node: Resource & Identifier;
+}
 interface ResourceInput {
   input: Resource;
 }
 
+interface ResourceInputWithId {
+  input: Resource & Identifier;
+}
+
+const RESOURCE_SUBSCRIPTION = 'resource_subscription';
+const RESOURCES_SUBSCRIPTION = 'resources_subscription';
+
 export default (pubsub: PubSub) => ({
-  Query: {},
-  Mutation: {
-    addResource:
-      // withAuth(
-      async (obj: any, { input }: ResourceInput, context: any) => {
-        try {
-          // console.log('input', input);
-          if (!input.userId) {
-            input.userId = context.identity.id;
-          }
-          const id = await context.Resources.addResource(input);
-          const resource = await context.Resources.resource(id);
-          // // publish for liswting list
-          //   pubsub.publish(LISTINGS_SUBSCRIPTION, {
-          //     listingsUpdated: {
-          //       mutation: "CREATED",
-          //       id,
-          //       node: listing
-          //     }
-          //   });
-          return resource;
-        } catch (e) {
-          return e;
+  Query: {
+    async resources(obj: any, { limit, after, orderBy, filter }: any, context: any) {
+      const edgesArray: Edges[] = [];
+      const { total, resources } = await context.Resources.resourcesPagination(limit, after, orderBy, filter);
+      const hasNextPage = total > after + limit;
+
+      resources.map((resource: Resource & Identifier, index: number) => {
+        edgesArray.push({
+          cursor: after + index,
+          node: resource
+        });
+      });
+      const endCursor = edgesArray.length > 0 ? edgesArray[edgesArray.length - 1].cursor : 0;
+
+      return {
+        totalCount: total,
+        edges: edgesArray,
+        pageInfo: {
+          endCursor,
+          hasNextPage
         }
-      }
-    // )
+      };
+    },
+    async resource(obj: any, { id }: Identifier, context: any) {
+      return context.Resources.resource(id);
+    }
   },
-  Subscription: {}
+  Mutation: {
+    addResource: withAuth(async (obj: any, { input }: ResourceInput, context: any) => {
+      try {
+        if (!input.userId) {
+          input.userId = context.identity.id;
+        }
+        const id = await context.Resources.addResource(input);
+        const resource = await context.Resources.resource(id);
+        // publish for resources list
+        pubsub.publish(RESOURCES_SUBSCRIPTION, {
+          resourcesUpdated: {
+            mutation: 'CREATED',
+            id,
+            node: resource
+          }
+        });
+        return resource;
+      } catch (e) {
+        return e;
+      }
+    }),
+    deleteResource: withAuth(async (obj: any, { id }: Identifier, context: any) => {
+      const resource = await context.Resources.resource(id);
+      const isDeleted = await context.Resources.deleteResource(id);
+      if (isDeleted) {
+        // publish for resource list
+        pubsub.publish(RESOURCES_SUBSCRIPTION, {
+          resourcesUpdated: {
+            mutation: 'DELETED',
+            id,
+            node: resource
+          }
+        });
+        // publish for edit resource page
+        pubsub.publish(RESOURCE_SUBSCRIPTION, {
+          resourceUpdated: {
+            mutation: 'DELETED',
+            id,
+            node: resource
+          }
+        });
+        return { id: resource.id };
+      } else {
+        return { id: null };
+      }
+    }),
+    editResource: withAuth(async (obj: any, { input }: ResourceInputWithId, context: any) => {
+      try {
+        await context.Resources.editResource(input);
+        const resource = await context.Resources.resource(input.id);
+        // publish for resource list
+        pubsub.publish(RESOURCES_SUBSCRIPTION, {
+          resourcesUpdated: {
+            mutation: 'UPDATED',
+            id: resource.id,
+            node: resource
+          }
+        });
+        // publish for edit resource page
+        pubsub.publish(RESOURCE_SUBSCRIPTION, {
+          resourceUpdated: {
+            mutation: 'UPDATED',
+            id: resource.id,
+            node: resource
+          }
+        });
+        return resource;
+      } catch (e) {
+        return e;
+      }
+    })
+  },
+  Subscription: {
+    resourceUpdated: {
+      subscribe: withFilter(
+        () => pubsub.asyncIterator(RESOURCE_SUBSCRIPTION),
+        (payload, variables) => {
+          return payload.resourceUpdated.id === variables.id;
+        }
+      )
+    },
+    resourcesUpdated: {
+      subscribe: withFilter(
+        () => pubsub.asyncIterator(RESOURCES_SUBSCRIPTION),
+        (payload, variables) => {
+          if (variables.endCursor) {
+            return variables.endCursor <= payload.resourcesUpdated.id;
+          } else {
+            return true;
+          }
+        }
+      )
+    }
+  }
 });
--- modules/resources/server-ts/sql.ts
+++ modules/resources/server-ts/sql.ts
@@ -1,6 +1,8 @@
+import { has } from 'lodash';
+import { Model, raw } from 'objection';
+import { camelizeKeys, decamelizeKeys, decamelize } from 'humps';
+
 import { knex } from '@gqlapp/database-server-ts';
-import { Model } from 'objection';
-import { camelizeKeys, decamelizeKeys } from 'humps';
 
 // Give the knex object to objection.
 Model.knex(knex);
@@ -18,9 +20,13 @@ export interface Resource {
   uploadedBy: string;
   tags: string;
   description: string;
-  resource: ResourceFiles;
+  resource: ResourceFiles[];
   createdAt: string;
   updatedAt: string;
+}
+
+export interface Identifier {
+  id: number;
 }
 
 const eager = '[resource]';
@@ -39,13 +45,63 @@ export default class Resources extends Model {
     return {
       resource: {
         relation: Model.HasManyRelation,
-        modelClass: ResourceDAO,
+        modelClass: ResourceFile,
         join: {
           from: 'resources.id',
           to: 'resource.resource_id'
         }
       }
     };
+  }
+
+  public async resourcesPagination(limit: number, after: number, orderBy: any, filter: any) {
+    const queryBuilder = Resources.query().eager(eager);
+    if (orderBy && orderBy.column) {
+      const column = orderBy.column;
+      let order = 'asc';
+      if (orderBy.order) {
+        order = orderBy.order;
+      }
+
+      queryBuilder.orderBy(decamelize(column), order);
+    } else {
+      queryBuilder.orderBy('id', 'desc');
+    }
+
+    if (filter) {
+      // if (has(filter, 'title') && filter.title !== '') {
+      //   queryBuilder.where(function() {
+      //     this.where('title', filter.title);
+      //   });
+      // }
+
+      // if (has(filter, 'uploadedBy') && filter.uploadedBy !== '') {
+      //   queryBuilder.where(function() {
+      //     this.where('uploaded_by', filter.uploadedBy);
+      //   });
+      // }
+
+      if (has(filter, 'tags') && filter.tags !== '') {
+        queryBuilder.where(function() {
+          this.where('tags', filter.tags);
+        });
+      }
+
+      if (has(filter, 'searchText') && filter.searchText !== '') {
+        queryBuilder
+          .from('resources')
+          .leftJoin('resources AS ld', 'ld.resources_id', 'resource.id')
+          .where(function() {
+            this.where(raw('LOWER(??) LIKE LOWER(?)', ['description', `%${filter.searchText}%`]))
+              .orWhere(raw('LOWER(??) LIKE LOWER(?)', ['title', `%${filter.searchText}%`]))
+              .orWhere(raw('LOWER(??) LIKE LOWER(?)', ['uploaded_by', `%${filter.searchText}%`]));
+          });
+      }
+    }
+    const allresources = camelizeKeys(await queryBuilder);
+    const total = allresources.length;
+    const res = camelizeKeys(await queryBuilder.limit(limit).offset(after));
+    return { resources: res, total };
   }
 
   public async resource(id: number) {
@@ -59,8 +115,14 @@ export default class Resources extends Model {
     return res;
   }
 
-  public resources() {
-    return knex.select();
+  public async resources(id: number) {
+    const res = camelizeKeys(
+      await Resources.query()
+        .findById(id)
+        .eager(eager)
+        .orderBy('id', 'desc')
+    );
+    return res;
   }
 
   public async addResource(params: Resource) {
@@ -69,9 +131,20 @@ export default class Resources extends Model {
 
     return res.id;
   }
+
+  public deleteResource(id: number) {
+    return knex('resources')
+      .where('id', '=', id)
+      .del();
+  }
+
+  public async editResource(params: Resource & Identifier) {
+    const res = await Resources.query().upsertGraph(decamelizeKeys(params));
+    return res.id;
+  }
 }
 
-class ResourceDAO extends Model {
+class ResourceFile extends Model {
   static get tableName() {
     return 'resource';
   }
