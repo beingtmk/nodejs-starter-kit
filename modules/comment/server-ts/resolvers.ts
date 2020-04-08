77721e874a5c60baf0cf8bb273e067b7d5d5d738
Bishal Deb <bishaldeb656@gmail.com>
2020-04-08 12:21:06 +0530

    Comment backend 1.0

--- modules/comment/server-ts/resolvers.ts
+++ modules/comment/server-ts/resolvers.ts
@@ -1,5 +1,156 @@
-export default (pubsub: any) => ({
-  Query: {},
-  Mutation: {},
-  Subscription: {}
+import { PubSub, withFilter } from 'graphql-subscriptions';
+import withAuth from 'graphql-auth';
+
+const C_REPLY_SUBSCRIPTION = 'reply_comment_subscription';
+const BLOG_C_SUBSCRIPTION = 'blog_comment_subscription';
+
+export default (pubsub: PubSub) => ({
+  Query: {
+    async comments(obj: any, args: any, context: any) {
+      return context.Comment.comments();
+    },
+    async blogComments(obj: any, { blogId }: any, { Comment }: any) {
+      return Comment.blogComments(blogId);
+    },
+    async comment(obj: any, { id }: any, context: any) {
+      return context.Comment.comment(id);
+    },
+    async commentReplies(obj: any, { referenceId }: any, context: any) {
+      return context.Comment.commentReplies(referenceId);
+    }
+  },
+  Mutation: {
+    addComment: withAuth(async (obj: any, { input }: any, { auth, Comment }: any) => {
+      try {
+        if (!input.userId) {
+          input.userId = auth.isAuthenticated.id;
+        }
+
+        const blogId = input.blogId;
+        const referenceId = input.referenceId;
+        delete input.blogId;
+        delete input.referenceId;
+
+        if (blogId && referenceId) {
+          throw new Error('Error in decision');
+        }
+        if (!blogId && !referenceId) {
+          throw new Error('Reference error');
+        }
+
+        const commentId = await Comment.addComment(input);
+        const item = await Comment.comment(commentId);
+
+        let data;
+        let id;
+        if (blogId) {
+          id = await Comment.addBlogComment({ blogId, commentId });
+          data = await Comment.blogComment(id);
+          pubsub.publish(BLOG_C_SUBSCRIPTION, {
+            blogCommentUpdated: {
+              mutation: 'CREATED',
+              node: data
+            }
+          });
+        } else {
+          id = await Comment.addReplyComment({ referenceId, commentId });
+          data = await Comment.replyComment(id);
+          pubsub.publish(C_REPLY_SUBSCRIPTION, {
+            replyCommentUpdated: {
+              mutation: 'CREATED',
+              node: data
+            }
+          });
+        }
+
+        return item;
+      } catch (e) {
+        return e;
+      }
+    }),
+    editComment: withAuth(async (obj: any, { input }: any, { Comment }: any) => {
+      try {
+        const ref = input.ref;
+        if (ref !== 'BLOG' && ref !== 'REPLY') {
+          throw new Error('Reference error');
+        }
+        delete input.ref;
+
+        await Comment.editComment(input);
+        const item = await Comment.comment(input.id);
+
+        let data;
+        if (ref === 'BLOG') {
+          data = await Comment.blogCommentWithCid(input.id);
+          pubsub.publish(BLOG_C_SUBSCRIPTION, {
+            blogCommentUpdated: {
+              mutation: 'UPDATED',
+              node: data
+            }
+          });
+        } else {
+          data = await Comment.replyCommentCid(input.id);
+          pubsub.publish(C_REPLY_SUBSCRIPTION, {
+            replyCommentUpdated: {
+              mutation: 'UPDATED',
+              node: data
+            }
+          });
+        }
+        return item;
+      } catch (e) {
+        return e;
+      }
+    }),
+    deleteComment: withAuth(async (obj: any, { id, ref }: any, { Comment }: any) => {
+      try {
+        const item = await Comment.comment(id);
+
+        let data;
+        if (ref === 'BLOG') {
+          data = await Comment.blogCommentWithCid(id);
+          await Comment.deleteBlogCommentWithCid(id);
+          pubsub.publish(BLOG_C_SUBSCRIPTION, {
+            blogCommentUpdated: {
+              mutation: 'DELETED',
+              node: data
+            }
+          });
+        } else {
+          data = await Comment.replyCommentCid(id);
+          await Comment.deleteReplyCommentWithCid(id);
+          pubsub.publish(C_REPLY_SUBSCRIPTION, {
+            replyCommentUpdated: {
+              mutation: 'DELETED',
+              node: data
+            }
+          });
+        }
+
+        await Comment.deleteComment(id);
+
+        return item;
+      } catch (e) {
+        return e;
+      }
+    })
+  },
+  Subscription: {
+    replyCommentUpdated: {
+      subscribe: withFilter(
+        () => pubsub.asyncIterator(C_REPLY_SUBSCRIPTION),
+        (payload, variables) => {
+          return payload.replyCommentUpdated.id === variables.id;
+        }
+      )
+    },
+    blogCommentUpdated: {
+      subscribe: withFilter(
+        () => pubsub.asyncIterator(BLOG_C_SUBSCRIPTION),
+        (payload, variables) => {
+          return payload.blogCommentUpdated.id === variables.id;
+        }
+      )
+    }
+  }
 });
--- modules/comment/server-ts/sql.ts
+++ modules/comment/server-ts/sql.ts
@@ -1,7 +1,237 @@
-import { knex } from '@gqlapp/database-server-ts';
+import {
+  camelizeKeys,
+  decamelizeKeys
+  //  decamelize
+} from 'humps';
+import {
+  Model
+  // raw
+} from 'objection';
+import {
+  knex
+  // returnId
+  //  orderedFor
+} from '@gqlapp/database-server-ts';
+import { User } from '@gqlapp/user-server-ts/sql';
+import Blog from '@gqlapp/blog-server-ts/sql';
+// import { has } from 'lodash';
 
-export default class Comment {
-  public comments() {
-    return knex.select();
+Model.knex(knex);
+
+// const eager = '[author.[profile], model]';
+const eager = '[user]';
+const blogEager = '[comment[user]]';
+const replyEager = '[comment[user]]';
+
+export default class Comment extends Model {
+  // private id: any;
+
+  static get tableName() {
+    return 'comment';
+  }
+
+  static get idColumn() {
+    return 'id';
+  }
+
+  static get relationMappings() {
+    return {
+      user: {
+        relation: Model.BelongsToOneRelation,
+        modelClass: User,
+        join: {
+          from: 'comment.user_id',
+          to: 'user.id'
+        }
+      }
+    };
+  }
+
+  public async comments() {
+    return camelizeKeys(
+      await Comment.query()
+        .eager(eager)
+        .orderBy('id', 'desc')
+    );
+  }
+
+  public async comment(id: number) {
+    const res = camelizeKeys(
+      await Comment.query()
+        .findById(id)
+        .eager(eager)
+        .orderBy('id', 'desc')
+    );
+    return res;
+  }
+
+  public async blogComments(id: number) {
+    return camelizeKeys(
+      await BlogComment.query()
+        .where({ blog_id: id })
+        .eager(blogEager)
+        .orderBy('id', 'desc')
+    );
+  }
+
+  public async blogComment(id: number) {
+    const res = camelizeKeys(
+      await BlogComment.query()
+        .findById(id)
+        .eager(blogEager)
+        .orderBy('id', 'desc')
+    );
+    return res;
+  }
+
+  public async blogCommentWithCid(id: number) {
+    const res = camelizeKeys(
+      await BlogComment.query()
+        .eager(blogEager)
+        .where({ comment_id: id })
+        .first()
+    );
+    return res;
+  }
+
+  public async commentReplies(id: number) {
+    return camelizeKeys(
+      await ReplyComment.query()
+        .where({ reference_id: id })
+        .eager(replyEager)
+        .orderBy('id', 'desc')
+    );
+  }
+
+  public async replyComment(id: number) {
+    const res = camelizeKeys(
+      await ReplyComment.query()
+        .findById(id)
+        .eager(replyEager)
+        .orderBy('id', 'desc')
+    );
+    return res;
+  }
+
+  public async replyCommentCid(id: number) {
+    const res = camelizeKeys(
+      await ReplyComment.query()
+        .eager(replyEager)
+        .where({ comment_id: id })
+        .first()
+    );
+    return res;
+  }
+
+  public async addComment(input: any) {
+    const res = await Comment.query().insertGraph(decamelizeKeys(input));
+    return res.id;
+  }
+
+  public async addBlogComment(input: any) {
+    const res = await BlogComment.query().insertGraph(decamelizeKeys(input));
+    return res.id;
+  }
+
+  public async addReplyComment(input: any) {
+    const res = await ReplyComment.query().insertGraph(decamelizeKeys(input));
+    return res.id;
+  }
+
+  public async editComment(id: number, input: any) {
+    const res = await Comment.query().upsertGraph(decamelizeKeys(input));
+    return res.id;
+  }
+
+  public async deleteComment(id: number) {
+    return knex('comment')
+      .where({ id })
+      .del();
+  }
+
+  public async deleteBlogComment(id: number) {
+    return knex('blog_comment')
+      .where({ id })
+      .del();
+  }
+
+  public async deleteBlogCommentWithCid(id: number) {
+    return knex('blog_comment')
+      .where({ comment_id: id })
+      .del();
+  }
+
+  public async deleteReplyComment(id: number) {
+    return knex('reply_comment')
+      .where({ id })
+      .del();
+  }
+
+  public async deleteReplyCommentWithCid(id: number) {
+    return knex('reply_comment')
+      .where({ comment_id: id })
+      .del();
+  }
+}
+
+class BlogComment extends Model {
+  static get tableName() {
+    return 'blog_comment';
+  }
+
+  static get idColumn() {
+    return 'id';
+  }
+
+  static get relationMappings() {
+    return {
+      blog: {
+        relation: Model.BelongsToOneRelation,
+        modelClass: Blog,
+        join: {
+          from: 'blog_comment.blog_id',
+          to: 'blog.id'
+        }
+      },
+      comment: {
+        relation: Model.BelongsToOneRelation,
+        modelClass: Comment,
+        join: {
+          from: 'blog_comment.comment_id',
+          to: 'comment.id'
+        }
+      }
+    };
+  }
+}
+
+class ReplyComment extends Model {
+  static get tableName() {
+    return 'reply_comment';
+  }
+
+  static get idColumn() {
+    return 'id';
+  }
+
+  static get relationMappings() {
+    return {
+      reference: {
+        relation: Model.BelongsToOneRelation,
+        modelClass: Comment,
+        join: {
+          from: 'reply_comment.reference_id',
+          to: 'comment.id'
+        }
+      },
+      comment: {
+        relation: Model.BelongsToOneRelation,
+        modelClass: Comment,
+        join: {
+          from: 'reply_comment.comment_id',
+          to: 'comment.id'
+        }
+      }
+    };
   }
 }
