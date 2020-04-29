281e18a9056733a7090460e74148641c777a42a7
Mithilesh Kohale <beingtmk@gmail.com>
2020-04-29 12:30:47 +0000

    Mailer edits & google oauth update

--- modules/mailer/server-ts/mailer.ts
+++ modules/mailer/server-ts/mailer.ts
@@ -2,12 +2,4 @@ import nodemailer from 'nodemailer';
 
 import settings from '@gqlapp/config';
 
-// export default nodemailer.createTransport(settings.mailer);
-
-export default nodemailer.createTransport({
-  service: 'gmail',
-  auth: {
-    user: 'nodejs.starter.kit@gmail.com',
-    pass: 'oneforall'
-  }
-});
+export default nodemailer.createTransport(settings.mailer);
