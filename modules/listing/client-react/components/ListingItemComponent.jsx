fc75332ca13d061ae1a6146045c2f531731fa959
Rishabh Kohale <r.kohale9@gmail.com>
2020-05-07 19:54:29 +0530

    minor changes

--- modules/listing/client-react/components/ListingItemComponent.jsx
+++ modules/listing/client-react/components/ListingItemComponent.jsx
@@ -64,59 +64,61 @@ class EventItemComponent extends Component {
       'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png';
 
     return (
-      <Card
-        title={<h3>{item.title}</h3>}
-        style={{ marginBottom: '24px' }}
-        bodyStyle={{
-          padding: '0px'
-        }}
-        extra={
-          <Row>
-            <Col span={12}>
-              <BorderListzero block onClick={() => history.push(`/edit/listing/${item.id}`)}>
-                <Icon type="edit" />
-              </BorderListzero>
-            </Col>
-            <Col span={12}>
-              <Popconfirm
-                title="Are you sure to delete this Listing?"
-                onConfirm={() => this.props.deleteProduct(item.id)}
-                onCancel={this.cancel}
-                okText="Yes"
-                cancelText="No"
-              >
-                <BorderListzero block>
-                  <Icon type="delete" />
+      <Link to={`/listing-detail/${item.id}`}>
+        <Card
+          title={<h3>{item.title}</h3>}
+          style={{ marginBottom: '24px' }}
+          hoverable
+          bodyStyle={{
+            padding: '0px'
+          }}
+          extra={
+            <Row>
+              <Col span={12}>
+                <BorderListzero block onClick={() => history.push(`/edit/listing/${item.id}`)}>
+                  <Icon type="edit" />
                 </BorderListzero>
-              </Popconfirm>
-            </Col>
-          </Row>
-        }
-      >
-        {' '}
-        <Row gutter={24} type="flex" justify="space-around" align="middle">
-          <Col
-            xs={{ span: 24 }}
-            md={{ span: 9 }}
-            xxl={{ span: 6 }}
-            align="center"
-            style={{ maxHeight: '200px', overflow: 'hidden' }}
-          >
-            <img alt="" src={item.listingImage[0].imageUrl} width="100%" />
-          </Col>
-          <Col xs={{ span: 24 }} md={{ span: 15 }} xxl={{ span: 18 }}>
-            <div
-              style={{
-                padding: '10px',
-                align: 'center',
-                height: '100%',
-                position: 'relative'
-              }}
+              </Col>
+              <Col span={12}>
+                <Popconfirm
+                  title="Are you sure to delete this Listing?"
+                  onConfirm={() => this.props.deleteProduct(item.id)}
+                  onCancel={this.cancel}
+                  okText="Yes"
+                  cancelText="No"
+                >
+                  <BorderListzero block>
+                    <Icon type="delete" />
+                  </BorderListzero>
+                </Popconfirm>
+              </Col>
+            </Row>
+          }
+        >
+          {' '}
+          <Row gutter={24} type="flex" justify="space-around" align="middle">
+            <Col
+              xs={{ span: 24 }}
+              md={{ span: 9 }}
+              xxl={{ span: 6 }}
+              align="center"
+              style={{ maxHeight: '200px', overflow: 'hidden' }}
             >
-              <h3>{`Event-Id: ${item.id}`}</h3>
-              <Divider style={{ margin: '5px 0px' }} />
+              <img alt="" src={item.listingImage[0].imageUrl} width="100%" />
+            </Col>
+            <Col xs={{ span: 24 }} md={{ span: 15 }} xxl={{ span: 18 }}>
+              <div
+                style={{
+                  padding: '10px',
+                  align: 'center',
+                  height: '100%',
+                  position: 'relative'
+                }}
+              >
+                <h3>{`Event-Id: ${item.id}`}</h3>
+                <Divider style={{ margin: '5px 0px' }} />
 
-              {/* <Link target="_blank" className="listing-link" to={`/public-profile/${user.id}`}>
+                {/* <Link target="_blank" className="listing-link" to={`/public-profile/${user.id}`}>
                 <Tooltip placement="topLeft" title="Visit User's Profile">
                   <Meta
                     avatar={<Avatar src={sellerAvatar} />}
@@ -130,32 +132,33 @@ class EventItemComponent extends Component {
                   />
                 </Tooltip>
               </Link> */}
-              <OrderTotalDate span={24}>
-                <h4>
-                  <OrderGrey sm={17} xs={24} />
-                  <OrderGrey sm={7} xs={24}>
-                    <span>
-                      <strong>
-                        <span>Total</span> &#8377; {item.listingCost.cost}
-                      </strong>
-                    </span>
-                  </OrderGrey>
-                </h4>
-              </OrderTotalDate>
-              <Row
-                gutter={13}
-                align="middle"
-                style={{
-                  position: 'absolute',
-                  bottom: '10px',
-                  left: '10px',
-                  right: '10px'
-                }}
-              ></Row>
-            </div>
-          </Col>
-        </Row>
-      </Card>
+                <OrderTotalDate span={24}>
+                  <h4>
+                    <OrderGrey sm={17} xs={24} />
+                    <OrderGrey sm={7} xs={24}>
+                      <span>
+                        <strong>
+                          <span>Total</span> &#8377; {item.listingCost.cost}
+                        </strong>
+                      </span>
+                    </OrderGrey>
+                  </h4>
+                </OrderTotalDate>
+                <Row
+                  gutter={13}
+                  align="middle"
+                  style={{
+                    position: 'absolute',
+                    bottom: '10px',
+                    left: '10px',
+                    right: '10px'
+                  }}
+                ></Row>
+              </div>
+            </Col>
+          </Row>
+        </Card>
+      </Link>
     );
   }
 }
