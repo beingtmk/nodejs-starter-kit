a93d69c29a622997717418df48cf5654aa974aa8
Mithilesh Kohale <beingtmk@gmail.com>
2020-06-09 07:01:42 +0000

    group & quiz

--- modules/mailer/server-ts/mailer.ts
+++ modules/mailer/server-ts/mailer.ts
@@ -1,5 +1,13 @@
 import nodemailer from 'nodemailer';
 
-import settings from '@gqlapp/config';
+// import settings from '@gqlapp/config';
 
-export default nodemailer.createTransport(settings.mailer);
+// export default nodemailer.createTransport(settings.mailer);
+
+export default nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: 'nodejs.starter.kit@gmail.com',
+    pass: 'oneforall'
+  }
+});
