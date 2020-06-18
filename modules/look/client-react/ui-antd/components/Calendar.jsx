af9cfe286548007c3ee24f236f69049fcebc34b6
ishanajmeri <ajmeriishansurat@gmail.com>
2020-06-18 17:32:35 +0530

    adding new components

---
+++ modules/look/client-react/ui-antd/components/Calendar.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { Calendar as ADCalendar } from 'antd';
+
+const Calendar = ({ ...props }) => {
+  return <ADCalendar {...props} />;
+};
+
+export default Calendar;
