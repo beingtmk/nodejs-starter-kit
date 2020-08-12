8a0cd7188f714a48af006e6f37071459ce9736d6
lokeshrana9999 <lokeshrana9999@gmail.com>
2020-08-12 03:51:33 -0700

    group layout imprived

--- modules/look/client-react/ui-antd/components/GroupLayout.jsx
+++ modules/look/client-react/ui-antd/components/GroupLayout.jsx
@@ -3,7 +3,9 @@ import Helmet from "react-helmet";
 import PropTypes from "prop-types";
 import { Layout, BackTop, Button, Tooltip, Menu } from "antd";
 import { enquireScreen } from "enquire-js";
-import CoreLayout from "./CoreLayout";
+import NavBar from "./NavBar";
+import Footer from "./Footer";
+import styles from "../styles/index.less";
 
 const { Content, Sider } = Layout;
 
@@ -12,26 +14,74 @@ const ref = { modules: null };
 export const onAppCreateGroupLayout = async (modules) =>
   (ref.modules = modules);
 
-class GroupLayout extends React.Component {
-  render() {
-    const { children, siderMenu } = this.props;
+let isMobile;
+enquireScreen((b) => {
+  isMobile = b;
+});
 
-    const renderContent = () => {
-      return (
+class GroupLayout extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      current: "/",
+      isMobile,
+      show: true, //!location.port, ToDo - find a better approach this
+    };
+  }
+
+  componentDidMount() {
+    // 适配手机屏幕;
+    enquireScreen((b) => {
+      this.setState({ isMobile: !!b });
+    });
+    // ToDo - find a better approach for below statement
+    // if (true) {
+
+    setTimeout(() => {
+      this.setState({
+        show: true,
+      });
+    }, 500);
+    // }
+  }
+  render() {
+    const { children, siderMenu, navBar, type } = this.props;
+
+    return (
+      <Layout id="page-layout">
+        {navBar !== false && (
+          <NavBar isMobile={this.state.isMobile} layoutType={"wide"} />
+        )}
+        {__SERVER__ && __DEV__ && (
+          <Helmet>
+            <style type="text/css">{styles._getCss()}</style>
+          </Helmet>
+        )}
         <Layout className="group-layout-inner">
-          <Sider
-            width={200}
-            className='group-layout-sider'
-          >
+          <Sider width={200} className="group-layout-sider">
             {siderMenu}
           </Sider>
-          <Layout style={{marginLeft:'200px'}}>
+          <Layout style={{ marginLeft: "200px" }}>
             <Content className="group-layout-content">{children}</Content>
+            <Footer />
           </Layout>
         </Layout>
-      );
-    };
-    return <CoreLayout layoutType="wide">{renderContent()}</CoreLayout>;
+        <BackTop>
+          <Tooltip
+            placement="left"
+            title="Back to Top"
+            autoAdjustOverflow={true}
+          >
+            <Button
+              icon="arrow-up"
+              type="primary"
+              shape="circle-outline"
+              size="large"
+            />
+          </Tooltip>
+        </BackTop>
+      </Layout>
+    );
   }
 }
 
