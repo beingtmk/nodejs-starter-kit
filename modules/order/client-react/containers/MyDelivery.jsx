854e34a7e0c32cdeea826905dc625e5084992c03
Rishabh Kohale <r.kohale9@gmail.com>
2020-09-25 01:17:02 +0530

    mydeliver query

--- modules/order/client-react/containers/MyDelivery.jsx
+++ modules/order/client-react/containers/MyDelivery.jsx
@@ -1,138 +1,39 @@
 import React, { useEffect } from 'react';
 import PropTypes from 'prop-types';
 import Helmet from 'react-helmet';
-import { Link } from 'react-router-dom';
-import { graphql } from 'react-apollo';
+import update from 'immutability-helper';
 
 import { compose } from '@gqlapp/core-common';
 import { translate } from '@gqlapp/i18n-client-react';
-import { Button, PageLayout } from '@gqlapp/look-client-react';
-import settings from '@gqlapp/config';
-import MyOrdersListView from '../components/MyDeliveryListView';
 
-import MY_DELIVERIES_QUERY from '../graphql/MyDeliveriesQuery.graphql';
+import { withCurrentUser, withOrders } from './OrderOperations';
 
-const ORDERS = [
-  {
-    id: 1,
-    orderDetails: [
-      {
-        id: 1,
-        thumbnail: 'https://res.cloudinary.com/nodejs-starter-kit/image/upload/v1582033916/ygz3yclqo2qmqewrqket.jpg',
-        title: 'Listing 1',
-        cost: 322,
-        date: 'Wed May 20 2020',
-        quantity: 4
-      },
-      {
-        id: 2,
-        thumbnail: 'https://res.cloudinary.com/nodejs-starter-kit/image/upload/v1582033916/ygz3yclqo2qmqewrqket.jpg',
-        title: 'Listing 2',
-        cost: 322,
-        date: 'Wed May 20 2020',
-        quantity: 3
-      }
-    ],
-    delivery: [
-      {
-        id: 1,
-        thumbnail: 'https://res.cloudinary.com/nodejs-starter-kit/image/upload/v1582033916/ygz3yclqo2qmqewrqket.jpg',
-        title: 'Listing 1',
-        cost: 322,
-        date: 'Wed May 20 2020',
-        quantity: 4
-      },
-      {
-        id: 2,
-        thumbnail: 'https://res.cloudinary.com/nodejs-starter-kit/image/upload/v1582033916/ygz3yclqo2qmqewrqket.jpg',
-        title: 'Listing 2',
-        cost: 322,
-        date: 'Wed May 20 2020',
-        quantity: 3
-      }
-    ]
-  },
-  {
-    id: 2,
-    orderDetails: [
-      {
-        id: 1,
-        thumbnail: 'https://res.cloudinary.com/nodejs-starter-kit/image/upload/v1582033916/ygz3yclqo2qmqewrqket.jpg',
-        title: 'Listing 1',
-        cost: 322,
-        date: 'Wed May 20 2020',
-        quantity: 4
-      },
-      {
-        id: 2,
-        thumbnail: 'https://res.cloudinary.com/nodejs-starter-kit/image/upload/v1582033916/ygz3yclqo2qmqewrqket.jpg',
-        title: 'Listing 2',
-        cost: 322,
-        date: 'Wed May 20 2020',
-        quantity: 3
-      }
-    ],
-    delivery: [
-      {
-        id: 1,
-        thumbnail: 'https://res.cloudinary.com/nodejs-starter-kit/image/upload/v1582033916/ygz3yclqo2qmqewrqket.jpg',
-        title: 'Listing 1',
-        cost: 322,
-        date: 'Wed May 20 2020',
-        quantity: 4
-      },
-      {
-        id: 2,
-        thumbnail: 'https://res.cloudinary.com/nodejs-starter-kit/image/upload/v1582033916/ygz3yclqo2qmqewrqket.jpg',
-        title: 'Listing 2',
-        cost: 322,
-        date: 'Wed May 20 2020',
-        quantity: 3
-      }
-    ]
-  }
-];
+// import MyDeliveryView from '../components/MyDeliveryView';
 
-const MyOrders = props => {
-  const renderMetaData = () => (
-    <Helmet
-      title={`${settings.app.name} - ${'MyOrders'}`}
-      meta={[
-        {
-          name: 'description',
-          content: `${settings.app.name} - ${''}`
-        }
-      ]}
-    />
-  );
-  console.log('admin blog', props);
+const MyDeliveryContainer = compose(withOrders)(props => {
+  console.log('props', props);
+  return React.cloneElement(props.children, { ...props });
+});
+
+const MyDelivery = props => {
+  const { currentUser } = props;
+
+  // console.log('props', props);
   return (
-    <PageLayout>
-      {renderMetaData()}
-
-      {props.loading ? <>Loading...</> : <MyOrdersListView orders={ORDERS} {...props} />}
-    </PageLayout>
+    <MyDeliveryContainer filter={{ vendorId: currentUser && currentUser.id }} {...props}>
+      <h1>hello</h1>
+      {/* <MyDeliveryView /> */}
+    </MyDeliveryContainer>
   );
 };
 
-// Orders.propTypes = {
-//   usersUpdated: PropTypes.object,
-//   updateQuery: PropTypes.func,
-//   t: PropTypes.func,
-//   subscribeToMore: PropTypes.func,
-//   filter: PropTypes.object
-// };
+MyDelivery.propTypes = {
+  //   usersUpdated: PropTypes.object,
+  //   updateQuery: PropTypes.func,
+  currentUser: PropTypes.func,
+  t: PropTypes.func
+  //   subscribeToMore: PropTypes.func,
+  //   filter: PropTypes.object
+};
 
-export default compose(
-  graphql(MY_DELIVERIES_QUERY, {
-    props({ data: { loading, error, userDeliveries } }) {
-      if (error) {
-        throw new Error(error);
-      }
-      console.log('******************');
-      console.log(userDeliveries);
-      return { loading, userDeliveries };
-    }
-  }),
-  translate('order')
-)(MyOrders);
+export default compose(withCurrentUser, translate('order'))(MyDelivery);
