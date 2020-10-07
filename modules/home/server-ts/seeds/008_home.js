6f7a9a22d20002a45a4339b4dd6a9ed1a61bccb2
Rishabh Kohale <r.kohale9@gmail.com>
2020-10-07 20:03:08 +0530

    seeds updated

--- modules/home/server-ts/seeds/008_home.js
+++ modules/home/server-ts/seeds/008_home.js
@@ -1,4 +1,5 @@
 import { returnId, truncateTables } from '@gqlapp/database-server-ts';
+import { LABEL } from '@gqlapp/home-common';
 
 exports.seed = async function(knex) {
   await truncateTables(knex, Promise, ['dynamic_carousel']);
@@ -8,7 +9,15 @@ exports.seed = async function(knex) {
       return returnId(knex('dynamic_carousel')).insert({
         image_url: `https://via.placeholder.com/2560x1600/141c1f?text=Nodejs${i + 1}`,
         link: null,
-        label: 'CAROUSEL',
+        label: LABEL[0],
+        is_active: true
+      });
+    }),
+    [...Array(3).keys()].map(async i => {
+      return returnId(knex('dynamic_carousel')).insert({
+        image_url: `https://via.placeholder.com/300x300/141c1f?text=Nodejs${i + 4}`,
+        link: null,
+        label: LABEL[1],
         is_active: true
       });
     })
