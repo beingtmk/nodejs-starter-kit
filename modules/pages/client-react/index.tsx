a15e13aad612c5ddf50c03d2f38551352ac564fa
Rishabh Kohale <r.kohale9@gmail.com>
2020-10-08 20:17:59 +0530

    static pages moved to pages module

--- modules/pages/client-react/index.tsx
+++ modules/pages/client-react/index.tsx
@@ -1,25 +1,48 @@
 import React from 'react';
+import {
+  Route
+  // , NavLink
+} from 'react-router-dom';
 
 import ClientModule from '@gqlapp/module-client-react';
-import { translate, TranslateFunction } from '@gqlapp/i18n-client-react';
+// import { translate, TranslateFunction } from '@gqlapp/i18n-client-react';
 import loadable from '@loadable/component';
+// import { MenuItem } from '@gqlapp/look-client-react';
 
-import { Route, NavLink } from 'react-router-dom';
-import { MenuItem } from '@gqlapp/look-client-react';
+import ROUTES from './routes';
 import resources from './locales';
 
-const NavLinkWithI18n = translate('pages')(({ t }: { t: TranslateFunction }) => (
-  <NavLink to="/pages" className="nav-link" activeClassName="active">
-    {t('pages:navLink')}
-  </NavLink>
-));
+// const NavLinkWithI18n = translate('pages')(({ t }: { t: TranslateFunction }) => (
+//   <NavLink to="/pages" className="nav-link" activeClassName="active">
+//     {t('pages:navLink')}
+//   </NavLink>
+// ));
 
 export default new ClientModule({
-  route: [<Route exact path="/pages" component={loadable(() => import('./containers/Pages').then(c => c.default))} />],
-  navItem: [
-    <MenuItem key="/pages">
-      <NavLinkWithI18n />
-    </MenuItem>
+  route: [
+    // <Route exact path="/pages" component={loadable(() => import('./containers/Pages').then(c => c.default))} />,
+    <Route
+      exact
+      path={ROUTES.aboutUs}
+      component={loadable(() => import('./containers/AboutUs').then(c => c.default))}
+    />,
+    <Route exact path={ROUTES.faq} component={loadable(() => import('./containers/Faq').then(c => c.default))} />,
+    <Route
+      exact
+      path={ROUTES.termsOfService}
+      component={loadable(() => import('./containers/TermsOfService').then(c => c.default))}
+    />,
+    <Route
+      exact
+      path={ROUTES.privacyPolicy}
+      component={loadable(() => import('./containers/PrivacyPolicy').then(c => c.default))}
+    />,
+    <Route exact path={ROUTES.email} component={loadable(() => import('./containers/Email').then(c => c.default))} />
   ],
+  // navItem: [
+  //   <MenuItem key="/pages">
+  //     <NavLinkWithI18n />
+  //   </MenuItem>,
+  // ],
   localization: [{ ns: 'pages', resources }]
 });
---
+++ modules/pages/client-react/routes/index.js
@@ -0,0 +1,9 @@
+const ROUTES = {
+  aboutUs: '/about-us',
+  faq: 'faq',
+  termsOfService: '/terms-of-service',
+  privacyPolicy: '/privacy-policy',
+  email: '/email'
+};
+
+export default ROUTES;
