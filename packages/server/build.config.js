826f5e53fa3c64f280c60dd03a091852d6c3dc00
lokeshrana9999 <lokeshrana9999@gmail.com>
2020-10-08 12:05:17 +0530

    url changed in server build config

--- packages/server/build.config.js
+++ packages/server/build.config.js
@@ -7,7 +7,7 @@ const config = {
   'process.env.NODE_ENV': process.env.NODE_ENV || 'development',
   __SERVER_PORT__: process.env.SERVER_PORT || process.env.PORT || 8080,
   __API_URL__: process.env.BACKEND_API_URL || '/graphql',
-  __WEBSITE_URL__: process.env.WEBSITE_URL || 'https://mapp.mithileshkohale.com',
+  __WEBSITE_URL__: process.env.WEBSITE_URL || 'https://mapp.brainayan.com',
   __CDN_URL__: process.env.CDN_URL || '',
   __FRONTEND_BUILD_DIR__: process.env.FRONTEND_BUILD_DIR || '../client/build'
 };
