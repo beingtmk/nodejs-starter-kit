3d253a31d5fe1ac37437504ca4604f642fff6be6
Rishabh Kohale <r.kohale9@gmail.com>
2020-11-27 19:16:15 +0530

    category navbar component updated

--- modules/category/client-react/components/CategoryNavBarComponentView.jsx
+++ modules/category/client-react/components/CategoryNavBarComponentView.jsx
@@ -1,23 +1,74 @@
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import ScrollParallax from 'rc-scroll-anim/lib/ScrollParallax';
-import { Menu } from 'antd';
+import { Menu, Row, Col } from 'antd';
 
 import { useImageLoaded } from '@gqlapp/listing-client-react/components/functions';
-import { Row, Col, DropDown, Card, Skeleton, Drawer, Icon, SubMenu, MenuItem, Button } from '@gqlapp/look-client-react';
+import {
+  /* Row, Col, */ DropDown,
+  Card,
+  Skeleton,
+  Drawer,
+  Icon,
+  SubMenu,
+  MenuItem,
+  Button
+} from '@gqlapp/look-client-react';
 import ROUTES from '@gqlapp/listing-client-react/routes';
 
 const CategoryNavBarComponentView = props => {
   const [ref, loaded, onLoad] = useImageLoaded();
   const [visible, setVisible] = useState(false);
+  const [showImg, setShowImg] = useState(true);
+  const [colWidth, setColWidth] = useState([]);
   const [drawerVisible, setDrawerVisible] = useState(false);
   const [activeCategory, setActiveCategory] = useState([]);
   const { mobile, loading, categories } = props;
 
   const setDropDownMenu = category => {
-    if (category.subCategories && category.subCategories.length > 0) {
+    const catLength = category.subCategories.filter(sC => sC.isNavbar && sC).length;
+    if (category.subCategories && catLength > 0) {
       setVisible(true);
       setActiveCategory(category.subCategories);
+      // if (catLength < 16) {
+      //   if (catLength < 8) {
+      //     setColWidth([...Array(catLength).keys()].map(() => 24 / catLength));
+      //   } else {
+      //     const isEvent = Boolean(!(catLength % 2));
+      //     if (isEvent) {
+      //       setColWidth([
+      //         ...[...Array(catLength / 2).keys()].map(() => 24 / parseInt(catLength / 2)),
+      //         ...[...Array(catLength / 2).keys()].map(() => 24 / parseInt(catLength / 2)),
+      //       ]);
+      //     } else {
+      //       console.log(catLength);
+      //       setColWidth([
+      //         ...[...Array(parseInt(catLength / 2)).keys()].map(() => parseInt(24 / parseInt(catLength / 2))),
+      //         ...[...Array(catLength - parseInt(catLength / 2)).keys()].map(() =>
+      //           parseInt(24 / (catLength - parseInt(catLength / 2)))
+      //         ),
+      //       ]);
+      //     }
+      //   }
+      // }
+      if (catLength < 16) {
+        setShowImg(true);
+        if (catLength < 8) {
+          setColWidth([...Array(catLength).keys()].map(() => 24 / catLength));
+        } else {
+          setColWidth([
+            ...[...Array(8).keys()].map(() => 3),
+            ...[...Array(catLength - 8).keys()].map(() => 24 / (catLength - 8))
+          ]);
+        }
+      } else {
+        setShowImg(false);
+        console.log([...Array(catLength % 8).keys()].map(() => 24 / (catLength % 8)));
+        setColWidth([
+          ...[...Array(catLength - (catLength % 8)).keys()].map(() => 3),
+          ...[...Array(catLength % 8).keys()].map(() => 24 / (catLength % 8))
+        ]);
+      }
     }
   };
 
@@ -96,64 +147,83 @@ const CategoryNavBarComponentView = props => {
         >
           {categories.edges &&
             categories.totalCount > 0 &&
-            categories.edges.map((c, i) => {
-              if (c.node.isNavbar) {
-                return (
-                  <Col key={i} span={24 / categories.totalCount}>
-                    <a
-                      href={`${ROUTES.categoryCatalogueLink}${c.node.id}`}
-                      onMouseEnter={() => setDropDownMenu(c.node)}
-                    >
-                      <h1>{c.node.title}</h1>
-                    </a>
-                  </Col>
-                );
-              }
-            })}
+            categories.edges
+              .filter(c => c.node.isNavbar && c)
+              .map((c, i) => {
+                if (c.node.isNavbar) {
+                  return (
+                    <Col key={i} span={24 / parseInt(categories.edges.filter(c => c.node.isNavbar && c).length)}>
+                      <a
+                        href="#"
+                        //  href={`${ROUTES.categoryCatalogueLink}${c.node.id}`}
+                        onMouseEnter={() => setDropDownMenu(c.node)}
+                      >
+                        <h1>{c.node.title}</h1>
+                      </a>
+                    </Col>
+                  );
+                }
+              })}
           <Col span={24} style={{ visibility: 'collapse' }}>
             <DropDown visible={visible} content={'navbar-category-dropdown'} className="navbar-category-dropdown">
-              <Row type="flex" justify="center" gutter={[24, 24]}>
-                {activeCategory.map(sC => (
-                  <Col span={3} align="center">
-                    <a href={`${ROUTES.categoryCatalogueLink}${sC.id}`}>
-                      <Card
-                        bordered={false}
-                        // style={{ border: '0px', borderRadius: '0px !important' }}
-                        bodyStyle={{
-                          margin: '0px',
-                          padding: '0px',
-                          textAlign: 'center'
-                        }}
-                        hoverable
-                        cover={
-                          <>
-                            {!loaded && (
-                              <div
-                                style={{
-                                  overflow: 'hidden',
-                                  height: '126px',
-                                  borderRadius: '8px 8px 0px 0px',
-                                  background: 'linear-gradient(90deg, #f2f2f2 25%, #e6e6e6 37%, #f2f2f2 63%)',
-                                  animation: 'ant-skeleton-loading 1.4s ease infinite'
-                                }}
-                                align="center"
-                              ></div>
-                            )}
-                            <img
-                              ref={ref}
-                              onLoad={onLoad}
-                              alt="example"
-                              src={sC.imageUrl}
-                              style={{ display: !loaded && 'none' }}
-                            />
-                          </>
-                        }
-                      >
-                        {sC.title}
-                      </Card>
-                    </a>
-                  </Col>
-                ))}
+              <Row type="flex" justify="center" gutter={[6, 6]}>
+                {activeCategory.map(
+                  (sC, idx) =>
+                    sC.isNavbar && (
+                      <Col span={colWidth[idx]} key={idx} align="center">
+                        <a href={`${ROUTES.categoryCatalogueLink}${sC.id}`}>
+                          <Card
+                            bodyStyle={{
+                              margin: '0px',
+                              padding: '0px'
+                            }}
+                            hoverable
+                          >
+                            <Card
+                              bordered={false}
+                              style={{ width: 'fit-content' /* border: '0px', borderRadius: '0px !important' */ }}
+                              bodyStyle={{
+                                // margin: showImg && '0px',
+                                padding: showImg && '0px',
+                                textAlign: 'center'
+                              }}
+                              // hoverable
+                              cover={
+                                showImg && (
+                                  <>
+                                    {!loaded && (
+                                      <div
+                                        style={{
+                                          overflow: 'hidden',
+                                          height: '126px',
+                                          borderRadius: '8px 8px 0px 0px',
+                                          background: 'linear-gradient(90deg, #f2f2f2 25%, #e6e6e6 37%, #f2f2f2 63%)',
+                                          animation: 'ant-skeleton-loading 1.4s ease infinite'
+                                        }}
+                                        align="center"
+                                      ></div>
+                                    )}
+                                    <img
+                                      ref={ref}
+                                      onLoad={onLoad}
+                                      alt="example"
+                                      src={sC.imageUrl}
+                                      style={{
+                                        // width: 'fit-content',
+                                        display: !loaded && 'none'
+                                      }}
+                                    />
+                                  </>
+                                )
+                              }
+                            >
+                              {sC.title}
+                            </Card>
+                          </Card>
+                        </a>
+                      </Col>
+                    )
+                )}
               </Row>
             </DropDown>
           </Col>
