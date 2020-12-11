8f6953ef05ce8742176829548f85f8910304b10a
Rishabh Kohale <r.kohale9@gmail.com>
2020-12-11 17:42:56 +0530

    layout extracted from cart

--- modules/order/client-react/components/CheckoutCartView.jsx
+++ modules/order/client-react/components/CheckoutCartView.jsx
@@ -1,32 +1,11 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import styled from 'styled-components';
-import { Link } from 'react-router-dom';
 
-import {
-  Heading,
-  PageLayout,
-  AddButton,
-  MetaTags,
-  Row,
-  Col,
-  Card,
-  Empty,
-  Divider,
-  Spinner
-} from '@gqlapp/look-client-react';
-// eslint-disable-next-line import/no-named-default
-import { default as LISTING_ROUTES } from '@gqlapp/listing-client-react/routes';
+import { PageLayout, MetaTags, Row, Col, Card, Divider, Spinner } from '@gqlapp/look-client-react';
 
-import CheckoutStepsComponent from './CheckoutStepsComponent';
 import OrderSummary from './OrderSummary';
+import CheckoutLayout from './CheckoutLayout';
 import CartItemComponent from './CartItemComponent';
-
-const CustomBody = styled.div`
-  background: white;
-  margin: 0 -200%;
-  padding: 0 200%;
-`;
 
 const CheckoutCartView = props => {
   const { t, history, cartLoading, onSubmit, getCart, onDelete, currentUser, onEdit } = props;
@@ -36,92 +15,61 @@ const CheckoutCartView = props => {
       <MetaTags title="Cart" description="meta" />
 
       {cartLoading && <Spinner />}
-      {!cartLoading &&
-        (getCart && getCart.orderDetails.length > 0 ? (
-          <div>
-            <Row type="flex">
-              <Col span={24} align="center">
-                <CheckoutStepsComponent step={0} t={t} />
-              </Col>
-              <Col span={24}>
-                <CustomBody>
-                  <br />
-                  <Heading>Shopping Cart</Heading>
-                  <br />
-                  <Row gutter={24}>
-                    <Col xxl={16} lg={16} xs={24}>
-                      <Card
-                        type="inner"
-                        title={
-                          <Row type="flex">
-                            <Col span={12}>
-                              <h3 style={{ marginBottom: '0px' }}>Product Details</h3>
-                            </Col>
-                            <Col span={12} style={{ display: 'flex' }}>
-                              <Col span={7} align="center">
-                                <h3 style={{ marginBottom: '0px' }}>Quantity</h3>
-                              </Col>
-                              <Col span={1} align="center" />
-                              <Col span={8} align="center">
-                                <h3 style={{ marginBottom: '0px' }}>Price</h3>
-                              </Col>
-                              <Col span={8} align="center">
-                                <h3 style={{ marginBottom: '0px' }}>Total</h3>
-                              </Col>
-                            </Col>
-                          </Row>
-                        }
-                        bodyStyle={{
-                          padding: '0px'
-                        }}
-                      >
-                        <br />
-                        {getCart &&
-                          getCart.orderDetails.map((cartItem, i) => (
-                            <Row>
-                              <Col span={24}>
-                                <CartItemComponent
-                                  modalId={cartItem.modalId}
-                                  t={t}
-                                  item={cartItem}
-                                  edit={true}
-                                  onSubmit={onSubmit}
-                                  onDelete={onDelete}
-                                  currentUser={currentUser}
-                                  onEdit={onEdit}
-                                />
-                                {getCart.orderDetails.length - 1 !== i ? <Divider /> : <br />}
-                              </Col>
-                            </Row>
-                          ))}
-                      </Card>
-                      <br />
-                      <Link className="listing-link" to={`${LISTING_ROUTES.listingCatalogue}`} target="_blank">
-                        <AddButton ghost block>
-                          Continue Shopping
-                        </AddButton>
-                      </Link>
+      <CheckoutLayout
+        t={t}
+        title={'Shopping Cart'}
+        loading={!cartLoading && getCart && getCart.orderDetails.length > 0}
+        Col1={
+          <>
+            <Card
+              type="inner"
+              title={
+                <Row type="flex">
+                  <Col span={12}>
+                    <h3 style={{ marginBottom: '0px' }}>Product Details</h3>
+                  </Col>
+                  <Col span={12} style={{ display: 'flex' }}>
+                    <Col span={7} align="center">
+                      <h3 style={{ marginBottom: '0px' }}>Quantity</h3>
                     </Col>
-                    <Col lg={8} sm={24} xs={24}>
-                      <OrderSummary t={t} getCart={getCart} history={history} />
+                    <Col span={1} align="center" />
+                    <Col span={8} align="center">
+                      <h3 style={{ marginBottom: '0px' }}>Price</h3>
+                    </Col>
+                    <Col span={8} align="center">
+                      <h3 style={{ marginBottom: '0px' }}>Total</h3>
+                    </Col>
+                  </Col>
+                </Row>
+              }
+              bodyStyle={{
+                padding: '0px'
+              }}
+            >
+              <br />
+              {getCart &&
+                getCart.orderDetails.map((cartItem, i) => (
+                  <Row>
+                    <Col span={24}>
+                      <CartItemComponent
+                        modalId={cartItem.modalId}
+                        t={t}
+                        item={cartItem}
+                        edit={true}
+                        onSubmit={onSubmit}
+                        onDelete={onDelete}
+                        currentUser={currentUser}
+                        onEdit={onEdit}
+                      />
+                      {getCart.orderDetails.length - 1 !== i ? <Divider /> : <br />}
                     </Col>
                   </Row>
-                  <br />
-                  <br />
-                  <br />
-                </CustomBody>
-              </Col>
-            </Row>
-          </div>
-        ) : (
-          <div className="centerAlign marginT30">
-            <Empty description="You have no items in your Cart">
-              <Link to={`${LISTING_ROUTES.listingCatalogue}`}>
-                <AddButton style={{ width: 'fit-content' }}>{t('checkoutCart.btn.add')}</AddButton>
-              </Link>
-            </Empty>
-          </div>
-        ))}
+                ))}
+            </Card>
+          </>
+        }
+        Col2={<OrderSummary t={t} getCart={getCart} history={history} />}
+      />
     </PageLayout>
   );
 };
---
+++ modules/order/client-react/components/CheckoutLayout.jsx
@@ -0,0 +1,70 @@
+import React from 'react';
+import styled from 'styled-components';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
+
+import { Empty, AddButton, Heading, Row, Col } from '@gqlapp/look-client-react';
+// eslint-disable-next-line import/no-named-default
+import { default as LISTING_ROUTES } from '@gqlapp/listing-client-react/routes';
+
+import CheckoutStepsComponent from './CheckoutStepsComponent';
+
+const CustomBody = styled.div`
+  background: white;
+  margin: 0 -200%;
+  padding: 0 200%;
+`;
+const CheckoutLayout = props => {
+  const { loading, t, title, Col1, Col2 } = props;
+  return loading ? (
+    <>
+      <Row type="flex">
+        <Col span={24} align="center">
+          <CheckoutStepsComponent step={0} t={t} />
+        </Col>
+        <Col span={24}>
+          <CustomBody>
+            <br />
+            <Heading>{title}</Heading>
+            <br />
+            <Row gutter={24}>
+              <Col xxl={16} lg={16} xs={24}>
+                {Col1}
+                <br />
+                <Link className="listing-link" to={`${LISTING_ROUTES.listingCatalogue}`} target="_blank">
+                  <AddButton ghost block>
+                    Continue Shopping
+                  </AddButton>
+                </Link>
+              </Col>
+              <Col lg={8} sm={24} xs={24}>
+                {Col2}
+              </Col>
+            </Row>
+            <br />
+            <br />
+            <br />
+          </CustomBody>
+        </Col>
+      </Row>
+    </>
+  ) : (
+    <div className="centerAlign marginT30">
+      <Empty description="You have no items in your Cart">
+        <Link to={`${LISTING_ROUTES.listingCatalogue}`}>
+          <AddButton style={{ width: 'fit-content' }}>{t('checkoutCart.btn.add')}</AddButton>
+        </Link>
+      </Empty>
+    </div>
+  );
+};
+
+CheckoutLayout.propTypes = {
+  loading: PropTypes.bool,
+  t: PropTypes.func,
+  title: PropTypes.string,
+  Col1: PropTypes.node,
+  Col2: PropTypes.node
+};
+
+export default CheckoutLayout;
