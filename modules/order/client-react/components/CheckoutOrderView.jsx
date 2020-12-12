186218814c6943a64d12d41684687ebcba874a27
Rishabh Kohale <r.kohale9@gmail.com>
2020-12-12 18:12:46 +0530

    checkoutorder ui update

--- modules/order/client-react/components/CheckoutOrderView.jsx
+++ modules/order/client-react/components/CheckoutOrderView.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Helmet from 'react-helmet';
-import { PageLayout, Row, Col, Card, Divider, Spinner } from '@gqlapp/look-client-react';
+import { Link } from 'react-router-dom';
 
-import AddressView from '@gqlapp/addresses-client-react/components/AddressView';
-
+import { PageLayout, Row, Col, NextButton, Card, Divider, Spinner, EditIcon } from '@gqlapp/look-client-react';
 import settings from '@gqlapp/config';
-import CheckoutStepsComponent from './CheckoutStepsComponent';
-import CheckoutCardComponent from './CheckoutCardComponent';
-import OrderTrackCardComponent from './OrderTrackCardComponent';
+
+import CheckoutLayout from './CheckoutLayout';
+import OrderSummary from './OrderSummary';
+import ListingCartItemComponent from './ListingCartItemComponent';
+import ROUTES from '../routes';
 
 const renderMetaData = () => (
   <Helmet
@@ -19,7 +20,7 @@ const renderMetaData = () => (
 );
 
 const CheckoutOrderView = props => {
-  const { t, getCart, getCartLoading, onSubmit } = props;
+  const { currentUser, t, getCart, cartLoading, onSubmit, history } = props;
 
   const address =
     getCart &&
@@ -31,58 +32,74 @@ const CheckoutOrderView = props => {
   return (
     <PageLayout>
       {renderMetaData()}
-      {getCartLoading && <Spinner />}
-      {!getCartLoading && (
-        <Row gutter={24}>
-          <Col span={24} align="center">
-            <CheckoutStepsComponent step={0} t={t} />
-            <br />
-            <br />
-          </Col>
-          <Col lg={22} md={22} xs={24}>
-            <Row gutter={24}>
-              <Col lg={10} md={10} xs={24}>
-                {getCart && (
-                  <Row gutter={24}>
+      {cartLoading && <Spinner />}
+
+      {!cartLoading && (
+        <CheckoutLayout
+          t={t}
+          title={'Select Address'}
+          loading={getCart && getCart.orderDetails.length > 0}
+          Col1={
+            <>
+              <Card type="inner" title={<h3 style={{ marginBottom: '0px' }}>Ship to name</h3>}>
+                <Row type={'flex'} align="middle">
+                  <Col span={22}>
+                    <h4>
+                      <strong>Shipping Address</strong>
+                    </h4>
+                    {`${address.streetAddress1}, ${address.streetAddress2}`}
+                    <br />
+                    {`${address.city}, ${address.state}`}
+                    <br />
+                    {`${address.pinCode}`}
+                    <br />
+                    {/* {`Mobile: ${address.user.mobile}`} */}
+                    {`Mobile: `}
+                  </Col>
+                  <Col span={2}>
+                    <br />
+                    <br />
+                    <br />
+                    <Link to={`${ROUTES.checkoutBill}`}>
+                      <EditIcon color="default" size="md" />
+                    </Link>
+                  </Col>
+                </Row>
+              </Card>
+              <br />
+              {getCart &&
+                getCart.orderDetails.map((cartItem, i) => (
+                  <Row>
                     <Col span={24}>
-                      <OrderTrackCardComponent
+                      <ListingCartItemComponent
+                        modalId={cartItem.modalId}
                         t={t}
-                        orderStatus={getCart.orderState}
-                        // status={state.status}
-                        completed={3}
+                        item={cartItem}
+                        edit={true}
+                        onSubmit={onSubmit}
+                        // onDelete={onDelete}
+                        currentUser={currentUser}
+                        // onEdit={onEdit}
                       />
-                      <Divider />
-                    </Col>
-                    <Col span={24}>
-                      <Card className="boxShadowTheme borderRadius9">
-                        <h4>{t('checkoutOrder.orderAddress')}</h4>
-                        <hr />
-                        <Row type="flex" justify="center" align="middle">
-                          {address && <AddressView addresses={[address]} addressId={address.id} />}
-                        </Row>
-                      </Card>
+                      {getCart.orderDetails.length - 1 !== i ? <Divider /> : <br />}
                     </Col>
                   </Row>
-                )}
-              </Col>
-              <Col lg={14} md={14} xs={24}>
-                {getCart && (
-                  <CheckoutCardComponent
-                    // onSubmit={openCheckout}
-                    t={t}
-                    onSubmit={onSubmit}
-                    getCart={getCart}
-                    product={{}}
-                    showState={false}
-                    showBtn={true}
-                    paid={true}
-                    buttonText={'Pay now'}
-                  />
-                )}
-              </Col>
-            </Row>
-          </Col>
-        </Row>
+                ))}
+            </>
+          }
+          Col2={
+            <OrderSummary
+              t={t}
+              getCart={getCart}
+              history={history}
+              btn={
+                <NextButton onClick={onSubmit} size="lg">
+                  PAY NOw
+                </NextButton>
+              }
+            />
+          }
+        />
       )}
     </PageLayout>
   );
@@ -90,7 +107,9 @@ const CheckoutOrderView = props => {
 
 CheckoutOrderView.propTypes = {
   getCart: PropTypes.object,
-  getCartLoading: PropTypes.bool,
+  history: PropTypes.object,
+  currentUser: PropTypes.object,
+  cartLoading: PropTypes.bool,
   onSubmit: PropTypes.func,
   t: PropTypes.func
 };
