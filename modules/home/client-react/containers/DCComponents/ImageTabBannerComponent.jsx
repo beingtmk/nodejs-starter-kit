eba0ddb847f00a0c3e0ed106ade39ed171605f88
Rishabh Kohale <r.kohale9@gmail.com>
2020-12-27 23:49:12 +0530

    home banner seed updated

--- modules/home/client-react/containers/DCComponents/ImageTabBannerComponent.jsx
+++ modules/home/client-react/containers/DCComponents/ImageTabBannerComponent.jsx
@@ -3,7 +3,6 @@ import PropTypes from 'prop-types';
 
 import { translate } from '@gqlapp/i18n-client-react';
 import { compose } from '@gqlapp/core-common';
-import { Spinner } from '@gqlapp/look-client-react';
 
 import { withDynamicCarousels } from './DynamicCarouselOperations';
 import ImageTabBannerComponentView from '../../components/DCComponents/ImageTabBannerComponentView';
@@ -14,9 +13,7 @@ const ImageTabBannerComponent = props => {
   // console.log('props', props);
   return !loading && dynamicCarousels ? (
     <ImageTabBannerComponentView {...props} id="Banner1_0" key="Banner1_0" data={dynamicCarousels} />
-  ) : (
-    <Spinner size="small" />
-  );
+  ) : null;
 };
 
 ImageTabBannerComponent.propTypes = {
